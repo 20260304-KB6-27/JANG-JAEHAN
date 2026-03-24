1392269099308fa563abc1b3f170e3d34581bd3c
JAEHAN-JANG <twinsjh01@gmail.com>
2026-03-24 19:48:09 +0900

    feat: 0324 실습과제 제출

---
+++ 03_Vue/01_basic/06_단일파일컴포넌트/test-vue-app2/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>APP Component</h2>
+    <hr />
+    <ul>
+      <CheckboxItem />
+      <CheckboxItem />
+      <CheckboxItem />
+      <CheckboxItem />
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import CheckboxItem from './components/CheckboxItem.vue';
+
+const checked = ref(false);
+</script>
+
+<style scoped></style>
