75423a43776816d5c22f411e0753ccd9c596f0fe
JAEHAN-JANG <twinsjh01@gmail.com>
2026-03-24 23:01:52 +0900

    fix: board_v3 addPost 0324

--- 03_Vue/02_advanced/06_단일파일컴포넌트/board_v3/src/components/InputPost.vue
+++ 03_Vue/02_advanced/06_단일파일컴포넌트/board_v3/src/components/InputPost.vue
@@ -1,7 +1,44 @@
 <template>
-  <div>입력 컴포넌트</div>
+  <div class="data-post">
+    <input
+      type="text"
+      placeholder="새 게시글 제목"
+      v-model="new_post.title"
+      @keyup.enter.exact="addPost"
+    />
+    <input
+      type="text"
+      placeholder="새 게시글 내용"
+      v-model="new_post.content"
+      @keyup.enter.exact="addPost"
+    />
+    <button @click="addPost">작성</button>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const new_post = ref({
+  title: "",
+  content: "",
+});
+
+const emit = defineEmits(["add"]);
+
+const addPost = () => {
+  if (new_post.value.title.trim() === "") return;
+  if (new_post.value.content.trim() === "") return;
+
+  emit("add", {
+    title: new_post.value.title,
+    content: new_post.value.content,
+  });
+
+  // post 초기화
+  new_post.value.title = "";
+  new_post.value.content = "";
+};
+</script>
 
 <style lang="scss" scoped></style>
